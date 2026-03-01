9f10dd1370c445fef7a1f7aaaf308e2bc307c7ec
king <kelvinozak0@gmail.com>
2026-03-01 18:26:47 +0100

    tech stack Modifications

--- src/components/ScrollRevealProvider.tsx
+++ src/components/ScrollRevealProvider.tsx
@@ -29,7 +29,7 @@ export default function ScrollRevealProvider() {
 
     return () => {
       clearTimeout(timer);
-      observ.disconnect();
+      observe.disconnect();
     };
   }, [pathname]);
 
